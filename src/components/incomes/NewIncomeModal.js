5f7966233dab11e81378935f56df6bb275e62803
Juan Ibarra <jmibarra86@gmail.com>
2021-10-14 19:06:45 -0300

    Se crea el form y se agrega al componente queda listo para comenzar a trabajar en el comportamiento

--- src/components/incomes/NewIncomeModal.js
+++ src/components/incomes/NewIncomeModal.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Button from '@mui/material/Button';
 
+import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
 
-import Box from '@mui/material/Box';
-
-import FormControl from '@mui/material/FormControl';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
+import IncomesForm from './IncomesForm';
 
 import * as incomeSelector from '../../store/incomes/reducer';
 import * as incomeActions from '../../store/incomes/actions';
@@ -37,30 +31,7 @@ const NewIncomeModal = (props) => {
             >
             <DialogTitle>{"Nuevo Ingreso"}</DialogTitle>
             <DialogContent>
-                <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                    <FormControl fullWidth sx={{ m: 1 }}>
-                    <InputLabel htmlFor="motivo-data-input">Motivo</InputLabel>
-                        <OutlinedInput
-                            id="motivo-data-input"
-                            label="Motivo"                        
-                        />
-                    </FormControl>
-                    <FormControl fullWidth sx={{ m: 1 }}>
-                        <InputLabel htmlFor="outlined-adornment-amount">Total</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-amount"
-                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                            label="Amount"
-                        />
-                    </FormControl>
-                </Box>
+                <IncomesForm/>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleCancel}>Cancelar</Button>
