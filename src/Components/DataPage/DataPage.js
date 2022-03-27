0bde08aab068090fb7ba177537c0525292ecdf17
Juan Ibarra <jmibarra86@gmail.com>
2022-03-26 21:08:05 -0300

    Experimento para ver como queda ocultar las métricas

--- src/Components/DataPage/DataPage.js
+++ src/Components/DataPage/DataPage.js
@@ -3,7 +3,10 @@ import {
     Col,
     Container,
     Row,
+    Button,
+    Collapse
 } from "reactstrap"
+import {AiFillSignal } from 'react-icons/ai';
 import ItemTableComponent from '../ItemTable'
 import ItemTCTableComponent from '../ItemTCTable'
 import MonthMetricsComponent from '../MonthMetrics'
@@ -14,6 +17,7 @@ const DataPage = ({year,month}) => {
     const [incomes, setIncomes] = useState([]);
     const [expenses, setExpenses] = useState([]);
     const [CCExpenses, setCCExpenses] = useState([]);
+    const [metricsOpen, setMetricsOpen] = useState(true)
 
     useEffect(()=> {
         fetchIncomesData(year,month);
@@ -48,23 +52,31 @@ const DataPage = ({year,month}) => {
     return (
         <>
             <Container>
-            <Row className="p-3 bg-dark my-2 rounded">
-                <MonthMetricsComponent incomes={incomes} expenses={expenses} creditcard={CCExpenses} year={year} month={month}/>
-            </Row>
-            <Row xs="2"> 
-                <Col>
-                    <h1>Ingresos</h1>
-                    <ItemTableComponent items={incomes} year={year} month={month} type="ingresos"/>
-                </Col>
-                <Col>
-                    <h1>Gastos</h1>
-                    <ItemTableComponent items={expenses} year={year} month={month} type="gastos"/>
-                </Col>          
-            </Row>
-            <Row>
-                <h1>Gastos Tarjeta de crédito</h1>
-                <ItemTCTableComponent items={CCExpenses} year={year} month={month} type="tc"/>
-            </Row>
+                <Row  p-3 my-2>
+                    <Col md={12}>
+                        <Button outline={metricsOpen} onClick={() => setMetricsOpen(!metricsOpen)}><AiFillSignal /> Metricas</Button>
+                    </Col>
+                </Row>
+
+                <Collapse isOpen={metricsOpen}>
+                    <Row className="p-3 bg-dark my-2 rounded">
+                        <MonthMetricsComponent incomes={incomes} expenses={expenses} creditcard={CCExpenses} year={year} month={month}/>
+                    </Row>
+                </Collapse>
+                <Row xs="2"> 
+                    <Col>
+                        <h1>Ingresos</h1>
+                        <ItemTableComponent items={incomes} year={year} month={month} type="ingresos"/>
+                    </Col>
+                    <Col>
+                        <h1>Gastos</h1>
+                        <ItemTableComponent items={expenses} year={year} month={month} type="gastos"/>
+                    </Col>          
+                </Row>
+                <Row>
+                    <h1>Gastos Tarjeta de crédito</h1>
+                    <ItemTCTableComponent items={CCExpenses} year={year} month={month} type="tc"/>
+                </Row>
             </Container>
             
         </>
