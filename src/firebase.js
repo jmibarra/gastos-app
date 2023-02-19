6fe43cd6b1c395b1293531a33187f986a58649ec
Juan Ibarra <jmibarra86@gmail.com>
2023-02-19 14:49:44 -0300

    Se modifica el código para que las credenciales esten en un archivo properties que no vamos a subir al repo

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,21 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
-import 'firebase/compat/auth'
+import 'firebase/compat/auth';
+import dotenv from 'dotenv';
 
-// Configura tus credenciales de Firebase
+dotenv.config();
+
 const firebaseConfig = {
-	apiKey: "AIzaSyCl6Cum-zKYBZbw-68bRUr1_yyIZv_2Lck",
-	authDomain: "gastos-2b065.firebaseapp.com",
-	databaseURL: "https://gastos-2b065-default-rtdb.firebaseio.com",
-	projectId: "gastos-2b065",
-	storageBucket: "gastos-2b065.appspot.com",
-	messagingSenderId: "683135440178",
-	appId: "1:683135440178:web:ac212a786cd2f12927b32b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Inicializa Firebase con la configuración
+console.log(process.env.REACT_APP_FIREBASE_DATABASE_URL)
 firebase.initializeApp(firebaseConfig);
 
-export default firebase 
-
+export default firebase;
